8aec951c75373b1f42fbcfe9f69ab70754678086
amykrisley <130614797+amykrisley@users.noreply.github.com>
2023-05-08 10:03:09 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3_ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b;
+            do
+            {
+                a++;
+                b = 5 * a;
+                Console.WriteLine("5 * {0} = {1}", a, b);
+            }
+            while (a < 10);
+        }
+    }
+}
